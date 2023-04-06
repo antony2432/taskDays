4ff6cd750c0244dd9b598f7d5f09212dfe421883
dean <achuquival@gmail.com>
2023-04-06 15:16:58 -0500

    agregando la page settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Root from "./pages/Root";
+import Settings from "./pages/Settings";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Root />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </div>
   );
